1a79e5f0bbcb24ef72c872034451afb5c72b69ba
yexin <yexin13@huawei.com>
2018-07-24 20:45:37 +0800

    Fix: build.sh failed in zsh
    Using build.sh in zsh environment has error,
    put `#!/bin/bash` in the first line can solve this.
    
    Change-Id: Ibf04662d72325bba276301791d43770e98a7fae8
    Signed-off-by: yexin <yexin13@huawei.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -x
 #########################################################################
 # Copyright (c) 2018 Huawei Technologies Co.,Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #########################################################################
-#!/bin/bash -x
 
 set -x
 
